e85528544a5fdd3c6efc8502e80d35bab6ad408c
María <kirilopez99@gmail.com>
2020-03-02 12:41:27 +0100

    más assets, modificadores y cosas ;)

--- Vamoos2/Assets/Scripts/Chip.cs
+++ Vamoos2/Assets/Scripts/Chip.cs
@@ -29,14 +29,5 @@ public class Chip : MonoBehaviour
         {
             PuertaGrandeAbierta = true;
         }
-
-        #region Camera
-        tr.rotation = Quaternion.LookRotation(transform.position - cam.position);
-        Vector3 look;
-        look.x = transform.position.x - cam.position.x;
-        look.y = 0;
-        look.z = transform.position.z - cam.position.z;
-        transform.rotation = Quaternion.LookRotation(look);
-        #endregion
     }
 }
---
+++ Vamoos2/Assets/Scripts/Modificador.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Modificador : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this.gameObject.tag
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
