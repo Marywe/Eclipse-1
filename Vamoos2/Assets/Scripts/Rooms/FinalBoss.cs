d79990a3a1f691b26c63aa95b3846bd532cd86ce
maria <kirilopez99@gmail.com>
2020-04-21 20:53:25 +0200

    Estados boss, pelea final

--- Vamoos2/Assets/Scripts/Rooms/FinalBoss.cs
+++ Vamoos2/Assets/Scripts/Rooms/FinalBoss.cs
@@ -5,7 +5,6 @@ using UnityEngine.Events;
 
 public class FinalBoss : MonoBehaviour
 {
-    public UnityEvent uwu;
     [SerializeField]
     private Transform[] ptosSpawn;
     [SerializeField]
@@ -15,36 +14,30 @@ public class FinalBoss : MonoBehaviour
 
     private int numBombas = 10;
 
-
-    public Collider sala;
     private Bounds limits;
-
+    public Collider sala;
     public Transform centroPozo;
+
+    public GameObject boss;
     // Start is called before the first frame update
     void Start()
     {
+       
         limits = sala.bounds;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void Entrada() //Que no se muevan
+    public void Entrada() //Que no se muevan
     {
         Controlador.instance.objetivo1.gameObject.GetComponent<CharacterController>().enabled = false;
         Controlador.instance.objetivo2.gameObject.GetComponent<CharacterController>().enabled = false;
 
         //REPRODUCIR RISA DEL PIBE
 
+        boss.SetActive(true);
         StartCoroutine(corEntrada());
     }
     void Phase1()
-    {
-        uwu.Invoke();
-        
+    { 
     }
 
     void Phase2()
@@ -53,7 +46,6 @@ public class FinalBoss : MonoBehaviour
     }
     IEnumerator corEntrada()
     {
-
         yield return new WaitForSeconds(3);
         Controlador.instance.objetivo1.gameObject.GetComponent<CharacterController>().enabled = true;
         Controlador.instance.objetivo2.gameObject.GetComponent<CharacterController>().enabled = true;
@@ -68,7 +60,7 @@ public class FinalBoss : MonoBehaviour
     void SpawnBombas()
     {
         float tiempoEntreBombas = 3;
-        for (int i = 0; i < numBombas; i++)
+        for (int i = 0; i < 3; i++)
         {
             Invoke("InstanciarBombas", tiempoEntreBombas);
             tiempoEntreBombas -= 0.1f;
@@ -78,8 +70,12 @@ public class FinalBoss : MonoBehaviour
     }
     void InstanciarBombas()
     {
-        Vector3 randomPosition = new Vector3(Random.Range(limits.min.x, limits.max.x), 0, Random.Range(limits.min.z, limits.max.z));
-        GameObject newBomba = Instantiate(prefabBomba, randomPosition, Quaternion.identity);        
+        for (int i = 0; i < numBombas; i++)
+        {
+            Vector3 randomPosition = new Vector3(Random.Range(limits.min.x, limits.max.x), 0, Random.Range(limits.min.z, limits.max.z));
+            GameObject newBomba = Instantiate(prefabBomba, randomPosition, Quaternion.identity);
+        }
+            
     }
 
     void BombaTocha()
