75f0aff1fd145b201a54b9eef5dc0628a23e525b
maria <kirilopez99@gmail.com>
2020-04-30 13:56:56 +0200

    Animator State Info ascensor

--- Vamoos2/Assets/Scripts/Rooms/ASCENSOR.cs
+++ Vamoos2/Assets/Scripts/Rooms/ASCENSOR.cs
@@ -2,20 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ASCENSOR : MonoBehaviour
+public class ASCENSOR : Salas
 {
     Animator ascensorAnim;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        salaClean = true;
+        salaCleanFirstTime = true;
+        numPuertas = 2;
+        ascensorAnim = transform.GetChild(0).GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (ascensorAnim.GetCurrentAnimatorStateInfo(0).IsTag("SUBIENDO") || ascensorAnim.GetCurrentAnimatorStateInfo(0).IsTag("BAJANDO"))
+        {
+            salaClean = false;
+        }
+        else salaClean = true;
+
+        base.ControladorPuertas(doors);
     }
 
 
